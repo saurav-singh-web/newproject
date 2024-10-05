949df60eac053dfb741943d3589c430cb68b96a6
saurav-singh-web <sauravsingh98866@gmail.com>
2024-10-05 22:13:40 +0530

    news website

---
+++ news/news.js
@@ -0,0 +1,643 @@
+let s = [
+  {
+    image:
+      "https://www.princeton.edu/sites/default/files/styles/1x_full_2x_half_crop/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=Bg2K7j7J",
+    chat: "1. Golden Retriever. It's safe to say that this breed is one of the most personable around. ",
+  },
+  {
+    image:
+      "https://www.princeton.edu/sites/default/files/styles/1x_full_2x_half_crop/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=Bg2K7j7J",
+    chat: "1. Golden Retriever. It's safe to say that this breed is one of the most personable around. ",
+  },
+  {
+    image:
+      "https://www.princeton.edu/sites/default/files/styles/1x_full_2x_half_crop/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=Bg2K7j7J",
+    chat: "1. Golden Retriever. It's safe to say that this breed is one of the most personable around. ",
+  },
+  {
+    image:
+      "https://www.princeton.edu/sites/default/files/styles/1x_full_2x_half_crop/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=Bg2K7j7J",
+    chat: "1. Golden Retriever. It's safe to say that this breed is one of the most personable around. ",
+  },
+  {
+    image:
+      "https://www.princeton.edu/sites/default/files/styles/1x_full_2x_half_crop/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=Bg2K7j7J",
+    chat: "1. Golden Retriever. It's safe to say that this breed is one of the most personable around. ",
+  },
+  {
+    image:
+      "https://www.princeton.edu/sites/default/files/styles/1x_full_2x_half_crop/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=Bg2K7j7J",
+    chat: "1. Golden Retriever. It's safe to say that this breed is one of the most personable around. ",
+  },
+  {
+    image:
+      "https://www.princeton.edu/sites/default/files/styles/1x_full_2x_half_crop/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=Bg2K7j7J",
+    chat: "1. Golden Retriever. It's safe to say that this breed is one of the most personable around. ",
+  },
+  {
+    image:
+      "https://www.princeton.edu/sites/default/files/styles/1x_full_2x_half_crop/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=Bg2K7j7J",
+    chat: "1. Golden Retriever. It's safe to say that this breed is one of the most personable around. ",
+  },
+  {
+    image:
+      "https://www.princeton.edu/sites/default/files/styles/1x_full_2x_half_crop/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=Bg2K7j7J",
+    chat: "1. Golden Retriever. It's safe to say that this breed is one of the most personable around. ",
+  },
+  {
+    image:
+      "https://www.princeton.edu/sites/default/files/styles/1x_full_2x_half_crop/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=Bg2K7j7J",
+    chat: "1. Golden Retriever. It's safe to say that this breed is one of the most personable around. ",
+  },
+  {
+    image:
+      "https://www.princeton.edu/sites/default/files/styles/1x_full_2x_half_crop/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=Bg2K7j7J",
+    chat: "1. Golden Retriever. It's safe to say that this breed is one of the most personable around. ",
+  },
+  {
+    image:
+      "https://www.princeton.edu/sites/default/files/styles/1x_full_2x_half_crop/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=Bg2K7j7J",
+    chat: "1. Golden Retriever. It's safe to say that this breed is one of the most personable around. ",
+  },
+];
+let a = [
+  {
+    image:
+      "https://www.mkewithkids.com/wp-content/uploads/2022/10/Pink-piglet.jpg",
+    chat: "The pig is considered an unclean animal as food in Judaism and Islam, and parts of Christianity.",
+  },
+  {
+    image:
+      "https://www.mkewithkids.com/wp-content/uploads/2022/10/Pink-piglet.jpg",
+    chat: "The pig is considered an unclean animal as food in Judaism and Islam, and parts of Christianity.",
+  },
+  {
+    image:
+      "https://www.mkewithkids.com/wp-content/uploads/2022/10/Pink-piglet.jpg",
+    chat: "The pig is considered an unclean animal as food in Judaism and Islam, and parts of Christianity.",
+  },
+  {
+    image:
+      "https://www.mkewithkids.com/wp-content/uploads/2022/10/Pink-piglet.jpg",
+    chat: "The pig is considered an unclean animal as food in Judaism and Islam, and parts of Christianity.",
+  },
+  {
+    image:
+      "https://www.mkewithkids.com/wp-content/uploads/2022/10/Pink-piglet.jpg",
+    chat: "The pig is considered an unclean animal as food in Judaism and Islam, and parts of Christianity.",
+  },
+  {
+    image:
+      "https://www.mkewithkids.com/wp-content/uploads/2022/10/Pink-piglet.jpg",
+    chat: "The pig is considered an unclean animal as food in Judaism and Islam, and parts of Christianity.",
+  },
+  {
+    image:
+      "https://www.mkewithkids.com/wp-content/uploads/2022/10/Pink-piglet.jpg",
+    chat: "The pig is considered an unclean animal as food in Judaism and Islam, and parts of Christianity.",
+  },
+];
+let u = [
+  {
+    image:
+      "https://i.ytimg.com/vi/CNn8c0wAPTE/hq720.jpg?sqp=-oaymwEXCK4FEIIDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDHDYYVjFWhYFH6BY10aB0GQVrSkg",
+    chat: "The pig is considered an unclean animal as food in Judaism and Islam, and parts of Christianity.",
+  },
+  {
+    image:
+      "https://i.ytimg.com/vi/CNn8c0wAPTE/hq720.jpg?sqp=-oaymwEXCK4FEIIDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDHDYYVjFWhYFH6BY10aB0GQVrSkg",
+    chat: "The pig is considered an unclean animal as food in Judaism and Islam, and parts of Christianity.",
+  },
+  {
+    image:
+      "https://i.ytimg.com/vi/CNn8c0wAPTE/hq720.jpg?sqp=-oaymwEXCK4FEIIDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDHDYYVjFWhYFH6BY10aB0GQVrSkg",
+    chat: "The pig is considered an unclean animal as food in Judaism and Islam, and parts of Christianity.",
+  },
+  {
+    image:
+      "https://i.ytimg.com/vi/CNn8c0wAPTE/hq720.jpg?sqp=-oaymwEXCK4FEIIDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDHDYYVjFWhYFH6BY10aB0GQVrSkg",
+    chat: "The pig is considered an unclean animal as food in Judaism and Islam, and parts of Christianity.",
+  },
+  {
+    image:
+      "https://i.ytimg.com/vi/CNn8c0wAPTE/hq720.jpg?sqp=-oaymwEXCK4FEIIDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDHDYYVjFWhYFH6BY10aB0GQVrSkg",
+    chat: "The pig is considered an unclean animal as food in Judaism and Islam, and parts of Christianity.",
+  },
+  {
+    image:
+      "https://i.ytimg.com/vi/CNn8c0wAPTE/hq720.jpg?sqp=-oaymwEXCK4FEIIDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDHDYYVjFWhYFH6BY10aB0GQVrSkg",
+    chat: "The pig is considered an unclean animal as food in Judaism and Islam, and parts of Christianity.",
+  },
+];
+let r = [
+  {
+    image: "https://images.indianexpress.com/2024/03/processed-food.jpg",
+    chat: "The pig is considered an unclean animal as food in Judaism and Islam, and parts of Christianity.",
+  },
+  {
+    image: "https://images.indianexpress.com/2024/03/processed-food.jpg",
+    chat: "The pig is considered an unclean animal as food in Judaism and Islam, and parts of Christianity.",
+  },
+  {
+    image: "https://images.indianexpress.com/2024/03/processed-food.jpg",
+    chat: "The pig is considered an unclean animal as food in Judaism and Islam, and parts of Christianity.",
+  },
+  {
+    image: "https://images.indianexpress.com/2024/03/processed-food.jpg",
+    chat: "The pig is considered an unclean animal as food in Judaism and Islam, and parts of Christianity.",
+  },
+  {
+    image: "https://images.indianexpress.com/2024/03/processed-food.jpg",
+    chat: "The pig is considered an unclean animal as food in Judaism and Islam, and parts of Christianity.",
+  },
+  {
+    image: "https://images.indianexpress.com/2024/03/processed-food.jpg",
+    chat: "The pig is considered an unclean animal as food in Judaism and Islam, and parts of Christianity.",
+  },
+];
+let b = [
+  {
+    chat: "About us",
+  },
+  {
+    chat: "Create Your Own Ad",
+  },
+  {
+    chat: "Terms of Use and Grievance Redressal Policy",
+  },
+  {
+    chat: "Privacy policy",
+  },
+  {
+    chat: "AAdvertise with us",
+  },
+  {
+    chat: "RSS",
+  },
+  {
+    chat: "Newsletter",
+  },
+  {
+    chat: "Feedback",
+  },
+  {
+    chat: "ePaper",
+  },
+  {
+    chat: "Sitemap",
+  },
+  {
+    chat: "Archives",
+  },
+];
+let c = [
+  {
+    chat: "The Economic Times",
+  },
+  {
+    chat: "Hindi Economic Times",
+  },
+  {
+    chat: "Navbharat Times",
+  },
+  {
+    chat: "Maharashtra Times",
+  },
+  {
+    chat: "Vijaya Karnataka",
+  },
+  {
+    chat: "Telugu Samayam",
+  },
+  {
+    chat: "Tamil Samayam",
+  },
+  {
+    chat: "Malayalam Samayam",
+  },
+  {
+    chat: "I am Gujarat",
+  },
+  {
+    chat: "TimesPoints",
+  },
+  {
+    chat: "Indiatimes",
+  },
+  {
+    chat: "Brand Capital",
+  },
+  {
+    chat: "Education Times",
+  },
+  {
+    chat: "Times Food",
+  },
+  {
+    chat: "Mumbai Mirror",
+  },
+  {
+    chat: "Bangalore Mirror",
+  },
+  {
+    chat: "MX Player",
+  },
+  {
+    chat: "Hindi Podcast",
+  },
+];
+let d = [
+  {
+    chat: "The Economic Times",
+  },
+  {
+    chat: "Hindi Economic Times",
+  },
+  {
+    chat: "Navbharat Times",
+  },
+  {
+    chat: "Maharashtra Times",
+  },
+  {
+    chat: "Vijaya Karnataka",
+  },
+  {
+    chat: "Telugu Samayam",
+  },
+  {
+    chat: "Tamil Samayam",
+  },
+  {
+    chat: "Malayalam Samayam",
+  },
+  {
+    chat: "I am Gujarat",
+  },
+  {
+    chat: "TimesPoints",
+  },
+];
+let e = [
+  {
+    chat: "The Economic Times",
+  },
+  {
+    chat: "Hindi Economic Times",
+  },
+  {
+    chat: "Navbharat Times",
+  },
+  {
+    chat: "Maharashtra Times",
+  },
+  {
+    chat: "Vijaya Karnataka",
+  },
+  {
+    chat: "Telugu Samayam",
+  },
+  {
+    chat: "Tamil Samayam",
+  },
+  {
+    chat: "Malayalam Samayam",
+  },
+  {
+    chat: "I am Gujarat",
+  },
+  {
+    chat: "TimesPoints",
+  },
+  {
+    chat: "Indiatimes",
+  },
+  {
+    chat: "Brand Capital",
+  },
+  {
+    chat: "Education Times",
+  },
+  {
+    chat: "Times Food",
+  },
+  {
+    chat: "Mumbai Mirror",
+  },
+  {
+    chat: "Bangalore Mirror",
+  },
+  {
+    chat: "MX Player",
+  },
+  {
+    chat: "Hindi Podcast",
+  },
+];
+let f = [
+  {
+    chat: "Privacy policy",
+  },
+  {
+    chat: "AAdvertise with us",
+  },
+  {
+    chat: "RSS",
+  },
+  {
+    chat: "Newsletter",
+  },
+  {
+    chat: "Feedback",
+  },
+  {
+    chat: "ePaper",
+  },
+  {
+    chat: "Sitemap",
+  },
+  {
+    chat: "Archives",
+  },
+];
+let g = [
+  {
+    chat: "About us",
+  },
+  {
+    chat: "Create Your Own Ad",
+  },
+  {
+    chat: "Terms of Use and Grievance Redressal Policy",
+  },
+  {
+    chat: "Privacy policy",
+  },
+  {
+    chat: "Advertise with us",
+  },
+  {
+    chat: "RSS",
+  },
+  {
+    chat: "Newsletter",
+  },
+  {
+    chat: "Feedback",
+  },
+  {
+    chat: "ePaper",
+  },
+  {
+    chat: "Sitemap",
+  },
+  {
+    chat: "Archives",
+  },
+];
+let h = [
+  {
+    chat: "The Economic Times",
+  },
+  {
+    chat: "Hindi Economic Times",
+  },
+  {
+    chat: "Navbharat Times",
+  },
+  {
+    chat: "Maharashtra Times",
+  },
+  {
+    chat: "Vijaya Karnataka",
+  },
+  {
+    chat: "Telugu Samayam",
+  },
+  {
+    chat: "Tamil Samayam",
+  },
+  {
+    chat: "Malayalam Samayam",
+  },
+  {
+    chat: "I am Gujarat",
+  },
+  {
+    chat: "TimesPoints",
+  },
+  {
+    chat: "Indiatimes",
+  },
+  {
+    chat: "Brand Capital",
+  },
+  {
+    chat: "Education Times",
+  },
+  {
+    chat: "Times Food",
+  },
+  {
+    chat: "Mumbai Mirror",
+  },
+  {
+    chat: "Bangalore Mirror",
+  },
+  {
+    chat: "MX Player",
+  },
+  {
+    chat: "Hindi Podcast",
+  },
+];
+let i = [
+  {
+    chat: "About us",
+  },
+  {
+    chat: "Create Your Own Ad",
+  },
+  {
+    chat: "Terms of Use and Grievance Redressal Policy",
+  },
+  {
+    chat: "Privacy policy",
+  },
+  {
+    chat: "Advertise with us",
+  },
+  {
+    chat: "RSS",
+  },
+  {
+    chat: "Newsletter",
+  },
+  {
+    chat: "Feedback",
+  },
+  {
+    chat: "ePaper",
+  },
+  {
+    chat: "Sitemap",
+  },
+  {
+    chat: "Archives",
+  },
+];
+let j = [
+  {
+    chat: "ICC Women’s T20 World Cup India schedule: Key",
+  },
+  {
+    chat: "ICC Women’s T20 World Cup India schedule: Key",
+  },
+  {
+    chat: "ICC Women’s T20 World Cup India schedule: Key",
+  },
+  {
+    chat: "ICC Women’s T20 World Cup India schedule: Key",
+  },
+  {
+    chat: "ICC Women’s T20 World Cup India schedule: Key",
+  },
+  {
+    chat: "ICC Women’s T20 World Cup India schedule: Key",
+  },
+  {
+    chat: "ICC Women’s T20 World Cup India schedule: Key",
+  },
+  {
+    chat: "ICC Women’s T20 World Cup India schedule: Key",
+  },
+  {
+    chat: "ICC Women’s T20 World Cup India schedule: Key",
+  },
+  {
+    chat: "ICC Women’s T20 World Cup India schedule: Key",
+  },
+  {
+    chat: "ICC Women’s T20 World Cup India schedule: Key",
+  },
+  {
+    chat: "ICC Women’s T20 World Cup India schedule: Key",
+  },
+  {
+    chat: "ICC Women’s T20 World Cup India schedule: Key",
+  },
+  {
+    chat: "ICC Women’s T20 World Cup India schedule: Key",
+  },
+  {
+    chat: "ICC Women’s T20 World Cup India schedule: Key",
+  },
+  {
+    chat: "ICC Women’s T20 World Cup India schedule: Key",
+  },
+  {
+    chat: "ICC Women’s T20 World Cup India schedule: Key",
+  },
+  {
+    chat: "ICC Women’s T20 World Cup India schedule: Key",
+  },
+];
+s.forEach(function (elem) {
+  let div = document.createElement("div");
+  let image = document.createElement("img");
+  image.setAttribute("src", elem.image);
+  let chat = document.createElement("p");
+  chat.innerText = elem.chat;
+  div.append(image, chat);
+  document.querySelector("#secondarynews").append(div);
+});
+a.forEach(function (elem) {
+  let div = document.createElement("div");
+  let image = document.createElement("img");
+  image.setAttribute("src", elem.image);
+  let chat = document.createElement("p");
+  chat.innerText = elem.chat;
+  div.append(image, chat);
+  document.querySelector("#frontn-2").append(div);
+});
+u.forEach(function (elem) {
+  let div = document.createElement("div");
+  let image = document.createElement("img");
+  image.setAttribute("src", elem.image);
+  let chat = document.createElement("h3");
+  chat.innerText = elem.chat;
+  div.append(image, chat);
+  document.querySelector("#travelhistory").append(div);
+});
+r.forEach(function (elem) {
+  let div = document.createElement("div");
+  let image = document.createElement("img");
+  image.setAttribute("src", elem.image);
+  let chat = document.createElement("h3");
+  chat.innerText = elem.chat;
+  div.append(image, chat);
+  document.querySelector("#foodhistory").append(div);
+});
+b.forEach(function (elem) {
+  let div = document.createElement("div");
+  let chat = document.createElement("h3");
+  chat.innerText = elem.chat;
+  div.append(chat);
+  document.querySelector("#logodiscription").append(div);
+});
+c.forEach(function (elem) {
+  let div = document.createElement("div");
+  let chat = document.createElement("h3");
+  chat.innerText = elem.chat;
+  div.append(chat);
+  document.querySelector("#othertimes").append(div);
+});
+d.forEach(function (elem) {
+  let div = document.createElement("div");
+  let chat = document.createElement("h3");
+  chat.innerText = elem.chat;
+  div.append(chat);
+  document.querySelector("#pularcat").append(div);
+});
+e.forEach(function (elem) {
+  let div = document.createElement("div");
+  let chat = document.createElement("h3");
+  chat.innerText = elem.chat;
+  div.append(chat);
+  document.querySelector("#ndingtopi").append(div);
+});
+f.forEach(function (elem) {
+  let div = document.createElement("div");
+  let chat = document.createElement("h3");
+  chat.innerText = elem.chat;
+  div.append(chat);
+  document.querySelector("#ewsitehea").append(div);
+});
+g.forEach(function (elem) {
+  let div = document.createElement("div");
+  let chat = document.createElement("h3");
+  chat.innerText = elem.chat;
+  div.append(chat);
+  document.querySelector("#rcatogaryde").append(div);
+});
+h.forEach(function (elem) {
+  let div = document.createElement("div");
+  let chat = document.createElement("h3");
+  chat.innerText = elem.chat;
+  div.append(chat);
+  document.querySelector("#dingtopicss-2").append(div);
+});
+i.forEach(function (elem) {
+  let div = document.createElement("div");
+  let chat = document.createElement("h3");
+  chat.innerText = elem.chat;
+  div.append(chat);
+  document.querySelector("#rvice-2").append(div);
+});
+j.forEach(function (elem) {
+  let div = document.createElement("div");
+  let chat = document.createElement("h3");
+  chat.innerText = elem.chat;
+  div.append(chat);
+  document.querySelector("#ter").append(div);
+});
